47c96c63f42cb13064c1a53fac550c1d51d48513
jwaiton <john.waiton@postgrad.manchester.ac.uk>
2025-04-11 17:51:47 +0100

    add cluster based scripts (11/04)

---
+++ cluster_analysis_code/HE_calibration/PSF_shenanigans/functions.py
@@ -0,0 +1,49 @@
+import os
+import glob
+import numpy  as np
+import pandas as pd
+import tables as tb
+import matplotlib.pyplot as plt
+
+
+def plot_z_bins(psf, bins, title, show = True):
+    plt.hist(psf.z, bins = bins)
+    plt.title(title)
+    plt.xlabel('z (mm)')
+    if show:
+        plt.show()
+
+
+def normalise(data):
+    return (data)/(max(data))
+
+
+def rr_process(df_in):    
+    return (df_in.assign(rr = (df_in.xr**2 + df_in.yr**2)**0.5)
+              .groupby("rr")
+              .agg({'factor' : "mean"})
+              .reset_index()
+           )
+
+
+def plot_psf(df, label, norm = False):
+
+    if norm:
+        plt.plot((np.flip(-df.rr)).append(df.rr), (np.flip(normalise(df.factor))).append(normalise(df.factor)), label = label)
+    else:
+        plt.plot((np.flip(-df.rr)).append(df.rr), (np.flip(df.factor)).append(df.factor), label = label)
+
+
+def z_specific_rr(df, z_val):
+    '''
+    Write script here that implements above rr function but across specific z value from the PSF.
+    '''
+    dfz = df[df.z == z_val]
+    return rr_process(dfz)
+
+
+def create_plots_psf(df, z, label = 'None', norm = False):
+    q = z_specific_rr(df, z)
+    plot_psf(q, label, norm)
+
+    return q
